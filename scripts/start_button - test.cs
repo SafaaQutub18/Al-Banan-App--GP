1465e6b14eb236dc48929fad25b75eb7ad5e337c
SafaaQutub18 <safaaqutub18@gmail.com>
2021-01-17 10:33:40 +0300

    add new scribt

---
+++ scripts/start_button - test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class start_button : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartApp()
+    {
+       //Application.LoadLevel(scene_name);
+       UnityEngine.SceneManagement.SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+
+    }
+
+}
