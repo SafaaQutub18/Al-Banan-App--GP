58901d6cf01105c2d68e7ebb6fc5b364eea06cc7
SafaaQutub18 <safaaqutub18@gmail.com>
2021-01-18 12:38:44 +0300

    update

---
+++ scripts/start_page.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class start_page : MonoBehaviour
+{
+    
+   public void StartApp(){
+       
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1 ); 
+
+   }
+}
