367f8ea1eb81eb69ce72a3e37935ad9c37df3929
Afraa Alandijani <afraakandijani@gmail.com>
2021-01-19 11:20:29 +0300

    add Sign in scripts

---
+++ scripts/Sign_In.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sign_In : MonoBehaviour
+{
+    
+   public void SignIn(){
+       
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1 ); 
+
+   }
+
+    public void CreateAccountPage(){
+       
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1 ); 
+
+   }
+}
