7f366db42998b86bd2424330aacf2f252bc8e9f7
nuha <nuhaharbi98@gmail.com>
2021-02-08 21:04:04 +0300

    add morphological code

--- scripts/Moropholgical_Analyzer.py
+++ scripts/Moropholgical_Analyzer.py
@@ -1,8 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb  7 22:26:44 2021
+from camel_tools.disambig.mle import MLEDisambiguator
 
-@author: nuhah
-"""
+# Load a pre-trained Maximum Likelihood Estimation model (MLE) disambiguator provided with CAMeL Tools.
+mle = MLEDisambiguator.pretrained()
 
+sentence = ['سوف', 'نقرأ', 'الكتب']
 
+# diacritize all words of sentence.
+max_likelihood_solutions = mle.disambiguate(sentence)
+
+# get morophlogical features of each word
+moropholgical_result = [features_of_word.analyses[0].analysis for features_of_word in max_likelihood_solutions]
