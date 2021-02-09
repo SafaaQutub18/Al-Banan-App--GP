69141739cf4e8d46772e7befe76ac1be20230ca2
Huda Alandijani <Hudak1419@gmail.com>
2021-02-09 17:39:28 +0300

    add uploude video

--- scripts/Upload_video.cs
+++ scripts/Upload_video.cs
@@ -2,32 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
 using UnityEditor;
+
+
 //using UnityEngine.Networking;
 
 public class Upload_video : MonoBehaviour
 {
-    string path; 
-    public RawImage rawImage;
+    
+    private VideoPlayer videoPlayer;
+    private VideoSource videoSource;
 
-    // Start is called before the first frame update
+    GameObject rawImage;
+    public GameObject playIcon;
+    private RenderTexture renderTexture;
+
+    // path of uploded video
+    public static string path; 
+
+    private bool isPaused = false;
+    private bool firstRun = true;
+
+    // Function of upload video button
     public void UploadVideo ()
     {
-       path = EditorUtility.OpenFilePanel("Show all images (.png)", "","png");  
-      GetImage ();
+       // Assign the video path selected by the user to the path variable
+       path = EditorUtility.OpenFilePanel("Show all images (.mp4)", "","mp4");  
+       Debug.Log("Done Uploading Video");
     }
 
-      public void GetImage () {
-        if (path != null) {
-            UpdateImage();
+    IEnumerator playVideo()
+    { 
+        playIcon.SetActive(false);
+        firstRun = false;
+
+        //Add VideoPlayer and rawImage to the GameObject
+        videoPlayer = gameObject.AddComponent<VideoPlayer>();
+        rawImage = GameObject.Find("RawImage");
+       
+        //Disable Play on Awake for both Video 
+        videoPlayer.playOnAwake = false;
+     
+        // get Vide clip from Url
+        videoPlayer.source = VideoSource.Url;
+        videoPlayer.url = path;
+        
+        //Set video To Play 
+        videoPlayer.Prepare();
+
+        //Wait until video is prepared
+        while (!videoPlayer.isPrepared)
+        {
+            yield return null;
+        }
+
+        Debug.Log("Done Preparing Video");
+        
+        //Assign the Texture from Video to RawImage to be displayed
+        rawImage.GetComponent<RawImage>().texture  = videoPlayer.texture;
+
+        //Play Video
+        videoPlayer.Play();
+        
+        Debug.Log("Playing Video");
+        while (videoPlayer.isPlaying)
+        {
+            Debug.LogWarning("Video Time: " + Mathf.FloorToInt((float)videoPlayer.time));
+            yield return null;
+        }
+
+        Debug.Log("Done Playing Video");
+    }
+
+    public void PlayPause() {
+        if(!firstRun && !isPaused) {
+            videoPlayer.Pause();
+            playIcon.SetActive(true);
+            isPaused = true;
+        } else if (!firstRun && isPaused) {
+            videoPlayer.Play();
+            playIcon.SetActive(false);
+            isPaused = false;
+        } else {
+            StartCoroutine(playVideo());
         }
     }
-    
-    public void UpdateImage () {
-        WWW www = new WWW("file:///" + path);
-        rawImage.texture = www.texture;
 
-    } 
 
-    
+
 }
+
