40b285730c81368095a455499b8abb874bb168f7
SafaaQutub18 <safaaqutub18@gmail.com>
2021-02-18 11:28:18 +0300

    modfcation on restructure scripts2

--- scripts/python_scripts/SpeechRecognition.py
+++ scripts/python_scripts/SpeechRecognition.py
@@ -1,6 +1,6 @@
 import azure.cognitiveservices.speech as speechsdk
 
-from Moropholgical_Analyzer import extractMorphologicalFeatures
+from Moropholgical_Analyzer import tokenizeText
     
 def convertSpeechToText(stop_recognition):
     global speech_recognizer
@@ -8,12 +8,11 @@ def convertSpeechToText(stop_recognition):
     if stop_recognition == True:    
         speech_recognizer.stop_continuous_recognition()
         
-        
     else:   
         speech_config = speechsdk.SpeechConfig(subscription="1fda37b270004643b89af5621d9902a2", region="eastasia", )
         speech_config.speech_recognition_language="ar-SA"
         speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
-        speech_recognizer.recognized.connect(lambda evt: extractMorphologicalFeatures(evt.result.text))
+        speech_recognizer.recognized.connect(lambda evt: tokenizeText(evt.result.text))
         speech_recognizer.start_continuous_recognition()
         
         
