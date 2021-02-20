40a491f5fc039a1064d663f0302a92a46a6263ce
Afraa Alandijani <afraakandijani@gmail.com>
2021-02-20 09:54:57 +0300

    update on connect python with c#

---
+++ scripts/Connect_python.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using System.Threading;
+
+public class Connect_python : MonoBehaviour
+{
+ Thread mThread;
+    public string connectionIP = "127.0.0.1";
+    public int connectionPort = 25001;
+    IPAddress localAdd;
+    TcpListener listener;
+    TcpClient client;
+    bool running = false;
+
+    public void Open()
+    {
+        if (running == true) {
+        listener.Stop();
+        Debug.Log("Stop listener");
+        running = false;
+        } else {
+        ThreadStart ts = new ThreadStart(GetInfo);
+        mThread = new Thread(ts);
+        mThread.Start();
+        Debug.Log("Start thread");
+        } 
+    }
+
+    void GetInfo()
+    {
+        localAdd = IPAddress.Parse(connectionIP);
+        listener = new TcpListener(IPAddress.Any, connectionPort);
+        listener.Start();
+        Debug.Log("Start listener");
+
+        client = listener.AcceptTcpClient();
+
+        running = true;
+        while (running)
+        {
+            SendAndReceiveData();
+        }
+    }
+
+    void SendAndReceiveData()
+    {
+        NetworkStream nwStream = client.GetStream();
+        byte[] buffer = new byte[client.ReceiveBufferSize];
+
+        //---receiving Data from the Host----
+        int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize); //Getting data in Bytes from Python
+        string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead); //Converting byte data to string
+
+        if (dataReceived != null)
+        { 
+           //---Using received data---
+            print(dataReceived);
+            print ("Helllooooooooo");
+        }
+
+    }
+    }
