e6336672e18fce6c988126a0298796b41da61c63
SafaaQutub18 <safaaqutub18@gmail.com>
2021-02-21 19:22:21 +0300

    clean files

--- scripts/python_scripts/WordsClass.py
+++
@@ -1,9 +0,0 @@
-
-class Words:
-    
-    
-     def __init__(self, word="", word_type=0,word_featuers_list=[]):
-         self.word=word
-         self.word_type= word_type
-         self.word_featuers_list=word_featuers_list
-    
