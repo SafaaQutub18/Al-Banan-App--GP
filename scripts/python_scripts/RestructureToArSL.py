34b5c2b905d3e4b3f2a72bead43bad0fd7898142
Huda Alandijani <Hudak1419@gmail.com>
2021-03-04 10:23:24 +0300

    add comments

--- scripts/python_scripts/RestructureToArSL.py
+++ scripts/python_scripts/RestructureToArSL.py
@@ -1,36 +1,43 @@
 from SearchInDictionary import checkText
 import re
-# filtering the compound word and word in text. 
+
+# filtering the text as follows: 
+# 1- Delete the extra word that have no signs in ArSL such as: prepositions, relative pronouns, punctuation marks
+# 2- Converting written numbers in text to digits
+# 3- put a mark of compound word that have on sign in ArSL and collect them in one index
+
 # parameter: the tokenized_text: which contains the list of word.
 #            the morphological_result: which contains the analyzed feature that extracted from the words.
 
-
- 
-
 def filteringText(tokenized_text,moropholgical_result,sock):
     # read the compound word file 
-    compound_word_file = open('compound word.txt', 'r', encoding="utf8",) 
+    compound_words_file = open('compound word.txt', 'r', encoding="utf8",) 
     
+    # declare the  prepositions, relative pronouns, punctuation marks, numbers
     jarr_letters = ["من", "عن", "على",  "حتي", "حتى", "في","الي" , "الى", "إلي", "إلى"] 
     asma_mosola = ["الذي" , "التي" , "اللذان" , "اللتان" , "الذين" , "اللتان" ,  "اللاتي" ,  "اللواتي" ,  "اللائي"]
     punctuation_marks = ["." , ":" ,  "،", "؟"]
     numbers = [('واحد',1), ('اثنين',2),('ثلاثة',3),('اربعة', 4),('خمسة', 5),('ستة', 6),('سبعة',7),('ثمانية', 8),('تسعة',9),('عشرة',10)
 ,('صفر',0), ('اثنان',2),('ثلاث',3),('اربع', 4),('خمس', 5),('ست', 6),('سبع',7),('ثمان', 8),('تسع',9),('عشر',10)]
+    
+    # list of text after filtering
     filtering_result = [] 
     counter = 0
-    
-    temp_compound_word=[]
+    # A temporary variable to store the word and check the next word to determine if these words are compound words or not 
+    temp_compound_words=[]
+    # list for collect the indexes that should deleted from the moropholgical_result list 
     delete_index = []
     
     # loop through the text word by word
     while counter < len(tokenized_text):
  
-        # check if text contains asma_mosola or jarr_letters to delete it
+        # check if text contains asma_mosola or jarr_letters or punctuation_marks to delete it
         if tokenized_text[counter] in asma_mosola or tokenized_text[counter] in jarr_letters or tokenized_text[counter] in punctuation_marks:
             delete_index.append(counter)
             counter+=1
             continue
         
+        # loop to replace each text number to digit
         for num in numbers:
             if tokenized_text[counter] == num[0]:
                moropholgical_result[counter]['lex'] = str(num[1])
@@ -39,55 +46,55 @@ def filteringText(tokenized_text,moropholgical_result,sock):
                break
         
         # start from the beginning of file
-        compound_word_file.seek(0)  
+        compound_words_file.seek(0)  
         
         # loop through the lines of file 
-        for line in compound_word_file:
+        for line in compound_words_file:
             
             # loop through the words of lines
             for word_in_line in line.split():
                 
-                #prevent counter from out of range exeption 
+                # prevent counter from out of range exeption 
                 if(counter == (len(tokenized_text))):
                    break
                
                 # check if the word in file match with the word in "tokenized_text" list
                 if word_in_line==tokenized_text[counter]:
                     
-                    #collect each word of the compound_word.
-                    temp_compound_word.append(word_in_line) 
+                    #collect each word of the compound words.
+                    temp_compound_words.append(word_in_line) 
                     
-                    # add the indexes of the word after the first word of compound word  to deleted
-                    if len(temp_compound_word) > 1:
+                    # check if temp_compound_words contains more than one word, then add the indexes of the word after the first word to delete_index list
+                    if len(temp_compound_words) > 1:
                        delete_index.append(counter)
                     counter+=1 
               
                     
             # after finishing each line check if there are compound word stored 
-            # in temp_compound_word, that for skip the rest of lines. 
-            if len(temp_compound_word) !=0:
+            # in temp_compound_words, that for skip the rest of lines. 
+            if len(temp_compound_words) !=0:
                     continue
         
-        #check if the temp_compound_word not empty add full compound word in the 
-        #filtering_result list , else the add the word filtering_result list. 
-        #that for keeping the order of the text.  
-        if len(temp_compound_word) > 1:
+        # check if the temp_compound_words not empty add full compound word in the 
+        # filtering_result list , else the add the word filtering_result list. 
+        # that for keeping the order of the text.  
+        if len(temp_compound_words) > 1:
             
             # special case for "السلام عليكم ورحمة الله وبركاته"
-            if " ".join(temp_compound_word) == "ورحمة الله وبركاته":
+            if " ".join(temp_compound_words) == "ورحمة الله وبركاته":
                 # add index with index of "ورحمة" before the index "الله وبركاته"
                 delete_index.insert(len(delete_index)-2, counter-2)
-                temp_compound_word=[]
+                temp_compound_words=[]
                 continue
             else: 
                 # add compound word to filtering result
-                filtering_result.append((" ".join(temp_compound_word), 1))
-                temp_compound_word=[]
+                filtering_result.append((" ".join(temp_compound_words), 1))
+                temp_compound_words=[]
                 
         # special case if word matched with part of compound word
-        elif len(temp_compound_word) == 1:
+        elif len(temp_compound_words) == 1:
             filtering_result.append((tokenized_text[counter-1], 0))
-            temp_compound_word=[]
+            temp_compound_words=[]
         else: 
             # add non compound word to filtering result
             filtering_result.append((tokenized_text[counter], 0))
