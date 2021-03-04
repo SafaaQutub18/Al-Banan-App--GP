fa93faeef34601c9ee29a52fe7ec17ee63006d91
Huda Alandijani <Hudak1419@gmail.com>
2021-03-04 10:05:54 +0300

    add comments

--- scripts/python_scripts/Moropholgical_Analyzer.py
+++ scripts/python_scripts/Moropholgical_Analyzer.py
@@ -4,24 +4,28 @@ from camel_tools.disambig.mle import MLEDisambiguator
 from camel_tools.tokenizers.word import simple_word_tokenize
 
 
+# function for tokenize text to word 
+# parameter: text that received from speech 
 def  tokenizeText(text ,sock):
     print(text)
 
     tokenized_text= simple_word_tokenize(text)
+    
     extractMorphologicalFeatures(tokenized_text,sock )
     
-    
+# fuction for extract morphological features for each word
+# parameter: tokenized_text 
 def extractMorphologicalFeatures(tokenized_text,sock):
     # Load a pre-trained Maximum Likelihood Estimation model (MLE) disambiguator provided with CAMeL Tools.
     mle = MLEDisambiguator.pretrained()
     
     # diacritize all words of sentence.
     max_likelihood_solutions = mle.disambiguate(tokenized_text)
-    #print(max_likelihood_solutions)
-    
-    # get morophlogical features of each word
+
+
+    # assign morophlogical features of each word in list 
     moropholgical_result = [features_of_word.analyses[0].analysis for features_of_word in max_likelihood_solutions]
-    print('-----------------------------------------')
+    
     filteringText(tokenized_text,moropholgical_result,sock)
     
 
--- scripts/python_scripts/SpeechRecognition.py
+++ scripts/python_scripts/SpeechRecognition.py
@@ -4,17 +4,19 @@ import azure.cognitiveservices.speech as speechsdk
 import socket
 import time
 
+# connect pythone with c#
 host, port = "127.0.0.1", 25001
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((host, port))
 
+# function to convert Speech-To-Text using azure-cognitiveservices-speech 
 def convertSpeechToText():
+    
         global sock
         speech_config = speechsdk.SpeechConfig(subscription="1fda37b270004643b89af5621d9902a2", region="eastasia", )
         speech_config.speech_recognition_language="ar-SA"
         speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
         speech_recognizer.recognized.connect(lambda evt:tokenizeText(evt.result.text ,sock))                    
-
         speech_recognizer.start_continuous_recognition()
         
         
