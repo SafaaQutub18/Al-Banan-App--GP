4a41c8b83caca3953179351aacc8f36fd5879534
Huda Alandijani <Hudak1419@gmail.com>
2021-03-05 18:38:37 +0300

    add restructer and search comments

--- scripts/python_scripts/RestructureToArSL.py
+++ scripts/python_scripts/RestructureToArSL.py
@@ -14,7 +14,7 @@ def filteringText(tokenized_text,moropholgical_result,sock):
     compound_words_file = open('compound word.txt', 'r', encoding="utf8",) 
     
     # declare the  prepositions, relative pronouns, punctuation marks, numbers
-    jarr_letters = ["من", "عن", "على",  "حتي", "حتى", "في","الي" , "الى", "إلي", "إلى"] 
+   
     asma_mosola = ["الذي" , "التي" , "اللذان" , "اللتان" , "الذين" , "اللتان" ,  "اللاتي" ,  "اللواتي" ,  "اللائي"]
     punctuation_marks = ["." , ":" ,  "،", "؟"]
     numbers = [('واحد',1), ('اثنين',2),('ثلاثة',3),('اربعة', 4),('خمسة', 5),('ستة', 6),('سبعة',7),('ثمانية', 8),('تسعة',9),('عشرة',10)
@@ -22,18 +22,24 @@ def filteringText(tokenized_text,moropholgical_result,sock):
     
     # list of text after filtering
     filtering_result = [] 
+    
+    # counter of while loop 
     counter = 0
+    
+    # counter of nested for loop 
     counter2=0
+    
     # A temporary variable to store the word and check the next word to determine if these words are compound words or not 
     temp_compound_words=[]
+    
     # list for collect the indexes that should deleted from the moropholgical_result list 
     delete_index = []
     
     # loop through the text word by word
     while counter < len(tokenized_text):
  
-        # check if text contains asma_mosola or jarr_letters or punctuation_marks to delete it
-        if tokenized_text[counter] in asma_mosola or tokenized_text[counter] in jarr_letters or tokenized_text[counter] in punctuation_marks:
+        # check if text contains asma_mosola  or punctuation_marks or part of speech is preposetion to delete it
+        if tokenized_text[counter] in asma_mosola or tokenized_text[counter] in punctuation_marks or moropholgical_result[counter]['pos'] =='prep':
             delete_index.append(counter)
             counter+=1
             continue
@@ -51,20 +57,23 @@ def filteringText(tokenized_text,moropholgical_result,sock):
         
         # loop through the lines of file 
         for line in compound_words_file:
+            
+            # assign the number of words in a line 
             word_num= len(line.split())
             counter2 = 0
+            
             # loop through the words of lines
             for word_in_line in line.split():
                 
                 # prevent counter from out of range exeption 
                 if(counter == (len(tokenized_text))):
                    break
-               
+               # check the first word of the line and break if it is not match with tokenized_text[counter]
                 if counter2 == 0 :
                     if word_in_line!=tokenized_text[counter]:
                         break
+                    
                 # check if the word in file match with the word in "tokenized_text" list
-                
                 if word_in_line==tokenized_text[counter]:
                     #collect each word of the compound words.
                     temp_compound_words.append(word_in_line) 
@@ -73,26 +82,29 @@ def filteringText(tokenized_text,moropholgical_result,sock):
                     if len(temp_compound_words) > 1:
                        delete_index.append(counter)
                     counter+=1
-                    print(counter2)
                 counter2+=1
+            
+            # check if word_num equal the temp_compound_words 
             if word_num == len(temp_compound_words):
                 break
                 
             # after finishing each line check if there are compound word stored 
-            # in temp_compound_words, that for skip the rest of lines. 
+            # in temp_compound_words, to skip the rest of lines. 
             if len(temp_compound_words) !=0:
                     break
         
         # check if the temp_compound_words not empty then add full compound word in the 
         # filtering_result list with 1 mark , else add the word in filtering_result list with '0' mark. 
         if len(temp_compound_words) > 1:
+            
             # special case for "السلام عليكم ورحمة الله وبركاته" compound word to make it equals to "السلام عليكم"
             if " ".join(temp_compound_words) == "ورحمة الله وبركاته":
-                # add index of "ورحمة" before the index of "الله وبركاته" to delete them, because index
-                 #  of "رحمة" not stored in line 69
+                
+                # add index of "ورحمة" before the index of "الله وبركاته" to delete them, because index of "رحمة" not stored in line 69
                 delete_index.insert(len(delete_index)-2, counter-2)
                 temp_compound_words=[]
                 continue
+            
             else:
                 # add compound words to filtering result
                 filtering_result.append((" ".join(temp_compound_words), 1))
@@ -120,15 +132,11 @@ def filteringText(tokenized_text,moropholgical_result,sock):
   
     
   
-# restructure the text as follows: 
-# 1- Delete the extra word that have no signs in ArSL such as: prepositions, relative pronouns, punctuation marks
-# 2- Converting written numbers in text to digits
-# 3- put a mark of compound word that have on sign in ArSL and collect them in one index
-
-# parameter: the tokenized_text: which contains the list of word.
+    
+# function to restructure the text into ArSL structure based on some rules  
+# parameter: the filtering_result: which contains the list of word after filtering.
 #            the morphological_result: which contains the analyzed feature that extracted from the words.
-  
-   
+     
 def restructureText(filtering_result, moropholgical_result ,sock):
         
     counter=0
@@ -139,7 +147,7 @@ def restructureText(filtering_result, moropholgical_result ,sock):
     # loop through all the words to produce final restructuring result
     for word in filtering_result: 
         
-        # skip the compound words becuse there are no need to restructure them.
+        # skip the compound words because there are no need to restructure them.
         if word[1]==1:
             restructured_text.append(word)
             counter+=1
@@ -155,10 +163,10 @@ def restructureText(filtering_result, moropholgical_result ,sock):
         if (re.sub("[^ك+_]","",features['atbtok'])) == 'ك+_' :
             restructured_text.append(("مثل",0))
          
-        # check if the POS equals noun or adj to add appropriate word depend on the features and cases  
+        # check if the POS equals noun or adjectiv or demonstrative pronoun to add appropriate word depend on the features and cases  
         if  features['pos']== 'noun' or features['pos']== 'adj' or features['pos']== 'pron_dem':
-            if features['num']== 's': # s= singler
-                 if features['gen']== 'f' and lemma != word[0] and features['rat']=='r' or features['rat']=='y': # r = rational
+            if features['num']== 's': # s= singular
+                 if features['gen']== 'f' and lemma != word[0] and features['rat']=='r' or features['rat']=='y': # r,y = rational
                         restructured_text.append((lemma,0))
                         restructured_text.append(("أنثى",0))
                         
@@ -169,7 +177,7 @@ def restructureText(filtering_result, moropholgical_result ,sock):
    # ********************************************************
 
             elif features['num']== 'd': # d= dual
-                if features['gen']== 'f' and re.search( 'ة', lemma ) == None and (features['rat']=='r' or features['rat']=='y'): # r = rational 
+                if features['gen']== 'f' and re.search( 'ة', lemma ) == None and (features['rat']=='r' or features['rat']=='y'): # r,y = rational , ex. the word "شمس" is female but not rational
                       
 
                       restructured_text.append((lemma,0))
@@ -183,7 +191,7 @@ def restructureText(filtering_result, moropholgical_result ,sock):
             # ********************************************************
 
             elif features['num']== 'p': # p = plural
-                if features['gen']== 'f' and re.search( 'ة', lemma ) == None and features['rat']=='r' or features['rat']=='y':  # r = rational
+                if features['gen']== 'f' and re.search( 'ة', lemma ) == None and features['rat']=='r' or features['rat']=='y':  # r,y = rational
                   restructured_text.append((lemma,0))
                   restructured_text.append(("كثير",0))
                   restructured_text.append(("أنثى",0))
@@ -193,33 +201,37 @@ def restructureText(filtering_result, moropholgical_result ,sock):
                     restructured_text.append(("كثير",0))
        
         # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        
         # check if the POS equals verb to add appropriate word depend on the features and cases 
-                    
         elif features['pos']== 'verb':
             if features['asp']== 'i': # i = imperfect (which means present or future tenses)
                if features['per']== '1' and features['num']== 'p': # per= person , 1 = first person (which means we or I) ,p = plural
                   restructured_text.append(("نحن",0))
-            #chexk if the verb start with 'سـ' letter to distinguish between the presen and future
+                  
+            #check if the verb start with 'سـ' letter to distinguish between the present and future
                if features['prc1']!= 'sa_fut': #sa_fut = 'سـ' future letter
                   restructured_text.append((lemma,2))
                else:
                   restructured_text.append((lemma,2))
                   restructured_text.append(("قريب",0))
-            else:
+                  
+            else: # other verb types 
                   restructured_text.append((lemma,2))
                             
         #check for Interrogative names (أسماء الاستفهام)                
         elif features['pos']=='adv_rel' or features['pos']=='pron_rel' or features['pos']=='adv_interrog' or features['pos']=='pron_interrog' or features['pos']=='part_interrog' :
             restructured_text.append(("استفهام",0))
             restructured_text.append((lemma,0))
-                
+            
+             #check for the digit
         elif features['pos']=='digit': 
-            restructured_text.append((re.sub("[^0-9]","",word[0]),3))            
+            restructured_text.append((re.sub("[^0-9]","",word[0]),3))  
+            
             #check for the arabic letters
         elif features['pos']=='abbrev': 
             restructured_text.append((lemma,4))
-        elif features['pos']=='prep':
-            restructured_text.append((word[0],0))             
+      
+            #any other type of pos
         else:
            restructured_text.append((lemma,0))
         counter+=1
--- scripts/python_scripts/SearchInDictionary.py
+++ scripts/python_scripts/SearchInDictionary.py
@@ -2,52 +2,75 @@
 import pandas as pd
 
 
-#Read files of each Dictionaries and 
+#Read each Dictionary files  
 compWord_file = pd.read_csv('compoundWords_Dictionary.csv',delimiter=',')
 letters_file = pd.read_csv('letters_Dictionary.csv',delimiter=',' )
 digits_file = pd.read_csv('digit_Dictionary.csv', delimiter=',' )
 words_file = pd.read_csv('words_Dictionary.csv',delimiter=',')
 verb_words_file = pd.read_csv('verbs_Dictionary.csv',delimiter=',')
 
-sign_id=0
 
-
+# function used for searching for the word in dictionary files, if it exists the sign id will be returned 
+# otherwise the system calls the “splitWordToLetters” or “splitDigit” functions to return the id of each letter or digit.
 def checkText(restructured_text ,sock):
-        
-    for word in restructured_text:        
+    
+    # loop through the restructured_text list     
+    for word in restructured_text:   
         if word[1] == 0: # check for words
             df_word=words_file[words_file.words.eq(word[0])]
             
+            # check if the word exist, return the word id 
             if words_file.words.eq(word[0]).any():
                 word_id = df_word['words_id'].iloc[0]
                 print(word_id)
                 word_id = (str(word_id)+", ")
+                
+                # send the word id to c# video listener script
                 sock.sendall(word_id.encode("UTF-8"))
                 
-            else:
+            else: # the word not exist, split word to letters
                 splitWordToLetters(word[0] ,sock)
                 
+              
         elif word[1] == 1: # check for compound words
+            
+            # search for compound words and return the compound words id
             df_compWord=compWord_file[compWord_file.compWord.eq(word[0])]
             compWord_id = df_compWord['compWord_id'].iloc[0]
             print(compWord_id)
             compWord_id=(str(compWord_id)+", ")
+            
+            # send the compound words id to c# video listener script
             sock.sendall(compWord_id.encode("UTF-8"))
             
         elif word[1] == 2: # check for verbs
             df_verb=verb_words_file[verb_words_file.verb_words.eq(word[0])]
             
+            # check if the verb exist, return the verb id 
             if verb_words_file.verb_words.eq(word[0]).any():
                 verb_id = df_verb['verb_id'].iloc[0]
                 print(verb_id)
                 verb_id = (str(verb_id)+", ")
+                
+                # send the verb id to c# video listener script
                 sock.sendall(verb_id.encode("UTF-8"))
                 
-            else:
+            else: # the verb not exist, split verb to letters
                 splitWordToLetters(word[0] ,sock)   
                 
         elif word[1] == 3: # check for digits
-            splitDigit(word[0],sock)
+        
+            df_digit=digits_file[digits_file.digits.eq(word[0])]
+            if digits_file.digits.eq(word[0]).any():
+                digit_id = df_digit['digit_id'].iloc[0]
+                print(digit_id)
+                digit_id = (str(digit_id)+", ")
+                
+                # send the digit id to c# video listener script
+                sock.sendall(digit_id.encode("UTF-8"))
+                
+            else: # the digit not exist, split digit 
+                  splitDigit(word[0],sock)
             
         elif word[1] == 4: # check for letters
             df_letter= letters_file[letters_file.letteres.eq(word[0])]
