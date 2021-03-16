94c5412e714c76f0525c0c5652e5d9d2c91c23f8
SafaaQutub18 <safaaqutub18@gmail.com>
2021-03-16 20:21:39 +0300

    EDIT list in translated video scene

--- scripts/TranslatedVideos.cs
+++ scripts/TranslatedVideos.cs
@@ -3,35 +3,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.IO;
+using UnityEngine.Video;
+
 
 public class TranslatedVideos : MonoBehaviour
 {
 	public GameObject prefab ; // This is our prefab object that will be exposed in the inspector
 	public int numberToCreate; // number of objects to create. Exposed in inspector
-    //GameObject rawImage;
-  //  public GameObject playIcon;
-//    private RenderTexture renderTexture;
+
+
+static string  path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),"RockVR/Video");
+static string[] files = System.IO.Directory.GetFiles( path );
 
 	void Start()
 	{  
-		Populate();
+		StartCoroutine(playVideo());
 	}
 	void Update()
 	{
 	}
-	void Populate()
+
+	private RenderTexture renderTexture;
+	private VideoPlayer videoPlayer;
+	private VideoSource videoSource;
+	
+  IEnumerator playVideo()
 	{
-        numberToCreate= 10;
+
+
+        numberToCreate= files.Length;
 		GameObject newObj ; // Create GameObject instance
 		for (int i = 0; i < numberToCreate; i++)
 		{
+			
+            renderTexture = new RenderTexture(160, 100, 24);
+			
+			videoPlayer = gameObject.AddComponent<VideoPlayer>();
+
 			 // Create new instances of our prefab until we've created as many as we specified
 			newObj = (GameObject)Instantiate(prefab, transform);
+
+			//Disable Play on Awake for both Video 
+			videoPlayer.playOnAwake = false;
+		
+			// get Vide clip from Url
+			videoPlayer.source = VideoSource.Url;
+			videoPlayer.url = files[i] ;
 			
-			// Randomize the color of our image
-			newObj.GetComponent<RawImage>().color = Random.ColorHSV();
+			//Set video To Play 
+			videoPlayer.Prepare();
+
+			//Wait until video is prepared
+			while (!videoPlayer.isPrepared)
+			{
+				yield return null;
+			}
+			//Assign the Texture from Video to RawImage to be displayed
+				newObj.GetComponent<RawImage>().texture  =renderTexture;
+				videoPlayer.renderMode = UnityEngine.Video.VideoRenderMode.RenderTexture;
+       
+             videoPlayer.targetTexture = renderTexture;
+			//Play Video
+			videoPlayer.Play();
+			
+			Debug.Log(files[i]);
              
 		}
+
 	}
+
+	
 
 }
